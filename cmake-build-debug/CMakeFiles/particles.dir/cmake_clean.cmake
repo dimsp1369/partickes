e286a24f41bfcd99954d8335c8c0ff6511500521
Spiridonov <dimsp1369@gmail.com>
2024-05-06 22:17:32 -0700

    Cmake config

---
+++ cmake-build-debug/CMakeFiles/particles.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/particles.dir/code/Engine.cpp.obj"
+  "CMakeFiles/particles.dir/code/Engine.cpp.obj.d"
+  "CMakeFiles/particles.dir/code/Matrices.cpp.obj"
+  "CMakeFiles/particles.dir/code/Matrices.cpp.obj.d"
+  "CMakeFiles/particles.dir/code/Particle.cpp.obj"
+  "CMakeFiles/particles.dir/code/Particle.cpp.obj.d"
+  "CMakeFiles/particles.dir/code/main.cpp.obj"
+  "CMakeFiles/particles.dir/code/main.cpp.obj.d"
+  "libparticles.dll.a"
+  "particles.exe"
+  "particles.exe.manifest"
+  "particles.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/particles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
